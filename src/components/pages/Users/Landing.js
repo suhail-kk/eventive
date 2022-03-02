96876fc5eecf0b7b2bbc200a63db5c06efff3958
suhail-kk <suhailkotta2001@gmail.com>
2022-03-02 08:26:16 +0530

    fwfw

--- src/components/pages/Users/Landing.js
+++ src/components/pages/Users/Landing.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Link, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+// import PropTypes from "prop-types";
 
-const Landing = ({ isAuthenticated }) => {
-	if (isAuthenticated) {
-		return <Redirect to="/dashboard" />;
-	}
+// const Landing = ({ isAuthenticated }) => {
+//   if (isAuthenticated) {
+//     return <Link to="/dashboard" />;
+//   }
 
-	return (
-		<section className="landing">
-			<div className="dark-overlay">
-				<div className="landing-inner">
-					<h1>Eventive</h1>
-					<p>Register, Login &amp; Logout Functionality with MERN</p>
-					<br />
-					<div className="buttons">
-						<Link to="/register" className="btn">
-							Sign Up
-						</Link>
-						<Link to="/login" className="btn">
-							Login
-						</Link>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
-};
+//   return (
+//     <section className="landing">
+//       <div className="dark-overlay">
+//         <div className="landing-inner">
+//           <h1>Eventive</h1>
+//           <p>Register, Login &amp; Logout Functionality with MERN</p>
+//           <br />
+//           <div className="buttons">
+//             <Link to="/register" className="btn">
+//               Sign Up
+//             </Link>
+//             <Link to="/login" className="btn">
+//               Login
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
